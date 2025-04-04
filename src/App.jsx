774d26e34df8130fc482e1dfd99364236192be29
rodriguez705 <rodriguezbrazzerss@gmail.com>
2025-04-03 18:45:56 -0700

    Agregue App.css y App.jsx y index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+import AnimalsShow from './AnimalsShow'
+
+function getRandomAnimal(){
+  const animals = ['cat', 'bird', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
+console.log(getRandomAnimal())
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [animals, setAnimals] = useState([])
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+  const renderAnimals = animals.map((animals, index) => {
+    return <AnimalsShow type={animals} key={index}/>
+
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animals-list">
+        {renderAnimals}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
